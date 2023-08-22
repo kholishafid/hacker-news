c3ca1c689fee6f3a358e8a3d980aee6dcf165e8c
kholishafid <itsmaehere@gmail.com>
2023-08-22 16:53:00 +0700

    remove stopPropagation

--- src/components/Story/HNStoryCommentCount.tsx
+++ src/components/Story/HNStoryCommentCount.tsx
@@ -3,8 +3,7 @@ import { MessageSquare } from "lucide-react";
 const HNStoryCommentCount = ({ commentCount }: { commentCount: number }) => {
   return (
     <div
-      className="text-xs flex items-center"
-      onClick={(e) => e.stopPropagation()}
+      className="text-xs flex items-center hover:underline w-fit"
     >
       <MessageSquare className="w-4 h-4 mr-1" />
       <span>{commentCount} reply</span>
